4a732d7b965f4cfd3ad2aa5221c41b161a108d92
Parth Pathak <85991449+parthpathakpp1@users.noreply.github.com>
2023-09-16 01:11:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from "cors"
 import path from 'path'
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 connectDB();
